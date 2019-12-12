1a1430f2dfd25f65ab0b889dd41ec4d5688869b9
남오승 <developernam92@gmail.com>
2019-12-12 19:56:13 +0900

    이미지Api 연

--- TestSearchingOS/src/main/java/com/test/search/HomeController.java
+++ TestSearchingOS/src/main/java/com/test/search/HomeController.java
@@ -2,10 +2,12 @@ package com.test.search;
 
 import java.text.DateFormat;
 import java.util.Date;
+import java.util.List;
 import java.util.Locale;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,8 +16,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 /**
  * Handles requests for the application home page.
  */
+
 @Controller
 public class HomeController {
+	
+	@Autowired
+	/* 각 상황의 타입에 맞는 IoC컨테이너 안에 존재하는 Bean을 자동으로 주입해주게 된다. */
+	HomeDao homeDao;
 	
 	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
 	
@@ -26,6 +33,30 @@ public class HomeController {
 	public String home(Locale locale, Model model) {
 		logger.info("Welcome home! The client locale is {}.", locale);
 		
+
+		List<HomeDto> list = homeDao.sel();
+
+		for(int i=0; i<list.size(); i++){
+
+			logger.info(list.get(i).getUser_id());
+
+
+			model.addAttribute("ID", list.get(0).getUser_id() );
+
+		}
+		
+		return "home";
+	}
+	
+
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/list", method = RequestMethod.GET)
+	public String getListApi(Locale locale, Model model) {
+		System.out.println("????");
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
 		Date date = new Date();
 		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
 		
@@ -33,7 +64,6 @@ public class HomeController {
 		
 		model.addAttribute("serverTime", formattedDate );
 		
-		return "home";
+		return "list";
 	}
-	
 }
